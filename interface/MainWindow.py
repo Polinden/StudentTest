641d4e59d38c3e34be8e1da602011bdf955fe150
polyna <you@example.com>
2018-12-07 12:36:19 +0200

    interface improved

--- interface/MainWindow.py
+++ interface/MainWindow.py
@@ -7,7 +7,6 @@ class MainFrame(Frame):
 
     def __init__(self, root):
         Frame.__init__(self, root)
-        self.subframe = None
         #main menu
         self.CreateMenu(root)
         #create screen
@@ -29,23 +28,16 @@ class MainFrame(Frame):
         #master
         menu=Menu(root)
         #first level
-        testmenu=Menu(menu, tearoff=0)
+        self.test_menu=testmenu=Menu(menu, tearoff=0)
         aboutmenu = Menu(menu, tearoff=0)
         #sublevel
         menu.add_cascade(label='Тест', menu=testmenu)
         menu.add_cascade(label='Про програму', menu=aboutmenu)
         testmenu.add_command(label='Відкрити тест', command=self.OpenFileMenu)
-        testmenu.add_command(label='Запамʼятати результати тестування', command=self.SaveFileMenu)
+        testmenu.add_command(label='Запамʼятати результати тестування', command=self.SaveFileMenu, state=DISABLED)
         testmenu.add_command(label='Вихід', command=root.quit)
         aboutmenu.add_command(label='Інформація', command=None)
         root.config(menu=menu)
-
-
-    def OpenFileMenu(self):
-        self.file=fdialog.askopenfile()
-
-    def SaveFileMenu(self):
-        self.file = fdialog.asksaveasfile()
 
 
     def CreateFrames(self):
@@ -57,49 +49,32 @@ class MainFrame(Frame):
         aw.grid(row=1, column=1, sticky=(N,S,E,W))
         rs = Frame(self, bd=1, relief=SUNKEN)
         rs.grid(row=2, column=0, columnspan=2, sticky=(N,S,E,W))
-        return {'question' : qw, 'test' : ts, 'answer' : aw, 'result' : rs}
+        sf = Frame(ts)
+        sf.grid(row=1, column=0, sticky=(N, W, S, E))
+        return {'question' : qw, 'test' : ts, 'answer' : aw, 'result' : rs, 'subframe':sf}
 
 
 
     def CreateCheckList(self):
         Label(self.frames['test'], text='Вiдповiдi', fg='green').grid(row=0, column=0, sticky=(N, W))
-        Button(self.frames['test'], text='  Вiдповiсти ', command=self.Answer).grid(row=2, column=0, sticky=(N,W),padx=(10, 10), pady=(30, 10))
-        self.subframe = Frame(self.frames['test'])
-        self.subframe.grid(row=1, column=0, sticky=(N, W, S, E))
+        self.answer_button=Button(self.frames['test'], text='  Вiдповiсти ', state=DISABLED, command=self.Answer)
+        self.answer_button.grid(row=2, column=0, sticky=(N,W),padx=(10, 10), pady=(30, 10))
         # scale to full screen (flex)
         self.AllRowColFlexible(self.frames['test'])
         # special case (not flex)
         self.frames['test'].grid_rowconfigure(0, weight=0)
-        self.frames['test'].grid_columnconfigure(0, weight=0)
         self.frames['test'].grid_rowconfigure(2, weight=0)
-
-
-    def SetCheckList(self, data=[], check=True):
-        #clear frame
-        self.ClearSubFrame(self.subframe)
-        result_set = []
-        result = IntVar(0)
-        for i, dat in enumerate(data):
-            Label(self.subframe, text=dat).grid(row=i, column=1, sticky=(N, W))
-            #tkinter type to link with check-radio
-            r=IntVar(0)
-            result_set.append(r)
-            if check:
-                Checkbutton(self.subframe, variable=r).grid(row=i, column=0, sticky=(N, S, E, W))
-            else:
-                Radiobutton(self.subframe, variable=result, value=i+1).grid(row=i, column=0, sticky=(N, S, E, W))
-        self.results=result_set if check else result
 
 
     def ClearSubFrame(self, sf):
         children=sf.grid_slaves()
         for child in children:
-            child.grid_forget()
             child.destroy()
 
 
     def CreateQuestion(self):
-        Label(self.frames['question'], text='Питання', fg='green').grid(row=0, column=0, sticky=(N, W))
+        self.question_lable=Label(self.frames['question'], text='Питання', fg='green')
+        self.question_lable.grid(row=0, column=0, sticky=(N, W))
         self.AllRowColFlexible(self.frames['question'])
         # special case (not flex)
         self.frames['question'].grid_rowconfigure(0, weight=0)
@@ -107,19 +82,11 @@ class MainFrame(Frame):
         self.qwest_message.grid(row=1, column=0, sticky=(N, S, W, E))
 
 
-    def SetQuestion(self, data='', image=None):
-        data = data.center(400, ' ')
-        self.qwest_message['text'] = data
-        if image:
-            pass
-        self.EnableNextButton(False)
-
-
     def CreateAnswer(self, data=''):
-        Label(self.frames['answer'], text='Правiльна вiдповiдь', fg='green').grid(row=0, column=0, columnspan=2,sticky=(N, W))
+        Label(self.frames['answer'], text='Правильна вiдповiдь', fg='green').grid(row=0, column=0, columnspan=2,sticky=(N, W))
         self.next_button=Button(self.frames['answer'], text='Продовжити', state=DISABLED, command=self.Next)
         self.next_button.grid(row=2, column=0, sticky=(S, W, N, E), padx=(10, 10),pady=(20, 10))
-        self.skip_button=Button(self.frames['answer'], text='Пропустити', command=self.Skip)
+        self.skip_button=Button(self.frames['answer'], text='Пропустити', state=DISABLED, command=self.Skip)
         self.skip_button.grid(row=2, column=1, sticky=(S, W, N, E), padx=(10, 10),pady=(20, 10))
         self.AllRowColFlexible(self.frames['answer'])
         self.frames['answer'].grid_rowconfigure(2, weight=0)
@@ -127,12 +94,57 @@ class MainFrame(Frame):
         self.answ_message.grid(row=1, column=0, columnspan=2, sticky=(N, W))
 
 
+
+    def SetCheckList(self, data=[], check=True):
+        #clear frame
+        self.ClearSubFrame(self.frames['subframe'])
+        result_set = []
+        result = IntVar(0)
+        for i, dat in enumerate(data):
+            Label(self.frames['subframe'], text=dat).grid(row=i, column=1, sticky=(N, W))
+            #tkinter type to link with check-radio
+            r=IntVar(0)
+            result_set.append(r)
+            if check:
+                Checkbutton(self.frames['subframe'], variable=r).grid(row=i, column=0, sticky=(N, S, E, W))
+            else:
+                Radiobutton(self.frames['subframe'], variable=result, value=i+1).grid(row=i, column=0, sticky=(N, S, E, W))
+        self.results=result_set if check else result
+        self.EnableSkipButton(bool(data))
+        self.EnableAnswertButton(bool(data))
+
+
+    def SetQuestion(self, data='', image=None):
+        data = data.center(400, ' ')
+        self.FinalViewOfQuestionFrame(False)
+        self.qwest_message['text'] = data
+        if image:
+            pass
+
     def SetAnswer(self, data=''):
+        self.EnableNextButton(bool(data))
         if data:
-            self.EnableNextButton(True)
+            self.EnableAnswertButton(False)
+            self.EnableSkipButton(False)
         data = data.center(200, ' ')
         self.answ_message['text'] = data
 
+
+    def SetFinalInfo(self, data=''):
+        self.SetCheckList()
+        self.SetAnswer()
+        self.SetQuestion(data)
+        self.FinalViewOfQuestionFrame(True)
+        self.test_menu.entryconfigure(1, state=NORMAL)
+
+
+    def FinalViewOfQuestionFrame(self, st):
+        if st:
+            self.question_lable['text'] = 'Ваши реузльтаты теста'
+            self.question_lable['fg'] = 'red'
+        else:
+            self.question_lable['text'] = 'Питання'
+            self.question_lable['fg'] = 'green'
 
 
     def EnableNextButton(self, st):
@@ -141,6 +153,8 @@ class MainFrame(Frame):
     def EnableSkipButton(self, st):
             self.skip_button.configure(state=NORMAL if st else DISABLED)
 
+    def EnableAnswertButton(self, st):
+        self.answer_button.configure(state=NORMAL if st else DISABLED)
 
     def CreateResults(self):
         Label(self.frames['result'], text='Результати').grid(row=0, column=0, sticky=(N, W, S))
@@ -149,9 +163,7 @@ class MainFrame(Frame):
 
 
 
-    def AllRowColFlexible(self, *frames, **allframes):
-        if allframes:
-            frames=list(allframes.values())
+    def AllRowColFlexible(self, *frames):
         for frame in frames:
             cols, rows =frame.grid_size()
             for i in range(rows):
@@ -160,15 +172,30 @@ class MainFrame(Frame):
                 frame.grid_columnconfigure(i, weight=1)
 
 
-    def RegisterListener(self, answer=None, next=None, skip=None):
+    def RegisterListener(self, answer=None, next=None, skip=None, open=None, save=None):
         self.answer=answer
         self.skip=skip
         self.next=next
+        self.open=open
+        self.save=save
+
+
+    def OpenFileMenu(self):
+        file=fdialog.askopenfile(filetypes=[('Xml files', '.xml')], title='Вiдкрити файл з тестом')
+        if file:
+            with file:
+                self.Open(file)
+
+    def SaveFileMenu(self):
+        file = fdialog.asksaveasfile(filetypes=[('Txt files', '.txt')], title='Обрати файл з результатом')
+        if file:
+            with file:
+                self.Save(file)
 
 
     def Answer(self):
         if self.CheckFilledResult():
-            if self.skip: self.answer(self)
+            if self.answer: self.answer(self)
 
     def Skip(self):
         if self.skip: self.skip(self)
@@ -176,17 +203,22 @@ class MainFrame(Frame):
     def Next(self):
         if self.next: self.next(self)
 
+    def Open(self, file):
+        if self.open: self.open(self, file)
+
+    def Save(self, file):
+        if self.save: self.save(self, file)
+
+
     def GetResults(self):
         try:
-            if type(self.results)==list:
-                return list(map(IntVar.get, self.results))
-            else: return self.results.get()
+            return list(map(IntVar.get, self.results)) if isinstance(self.results, list) else self.results.get()
         except AttributeError:
             return 0
 
     def CheckFilledResult(self):
         res=self.GetResults()
-        if type(res)==int:
+        if isinstance(res, int):
             res=[res]
         if sum(res)==0:
             return False
@@ -194,45 +226,56 @@ class MainFrame(Frame):
             return True
 
 
-main_frame=None
-def StartGUI(answer=None, next=None, skip=None):
+#thisd is an entry point
+#only give it your listeners
+def StartGUI(answer=None, next=None, skip=None, open=None, save=None):
     root=Tk()
     root.title('Тестування для ліцеїстів')
     root.geometry('700x500')
-    global main_frame
     main_frame=MainFrame(root)
-    main_frame.RegisterListener(answer, next, skip)
+    main_frame.RegisterListener(answer, next, skip, open, save)
     mainloop()
 
 
 
 
 
+###############
+#For Tests Only
+###############
 
-def Test(self):
-    questions=[(('Почему все вот так?'), ('Когда?', 'Где?', 'Что?', 'Когда?', 'Где?', 'Что?')),
-               (('Зачем и почему все вот так?'), ('Где?', 'Что?', 'Когда?', 'Где?', 'Что?')),
+def Test_Question(mf):
+    r = random.randint(0, 10)
+    if r>7:
+        mf.SetFinalInfo(' Вы вааще не правы!')
+        return
+    questions=[(('Почему все вот так, а у рыбы хвост?'), ('Когда?', 'Где?', 'Что?', 'Когда?', 'Где?', 'Что?')),
+               (('Зачем и почему все вот так, учишь-учшиь, а ничего не понимаешь?'), ('Где?', 'Что?', 'Когда?', 'Где?', 'Что?')),
                (('Зачем козлу борода и почему все вот так?'), ('И чо?', 'Что?', 'Где?', 'Что?', 'Когда?', 'Где?', 'Что?')),
                (('Почему все вот так, а не так и все вот так, а не сяк или Почему все вот так, а не так и все вот так, а не сяк?'),
                      ('Когда?', 'Где?', 'Что?', 'Когда?', 'Где?', 'Что?', 'Когда?', 'Где?', 'Что?'))]
     mode=[True, True, False, False]
     random.shuffle(mode)
     random.shuffle(questions)
-    self.SetCheckList(questions[0][1], mode[0])
-    self.SetQuestion(questions[0][0])
-    self.SetAnswer()
+    mf.SetCheckList(questions[0][1], mode[0])
+    mf.SetQuestion(questions[0][0])
+    mf.SetAnswer()
 
-def Test1(self):
+def Test_Answer(mf):
     answer=['Глупо ', 'Тупо ', 'Умно ', 'Нудно ']
     random.shuffle(answer)
-    str_res=self.GetResults()
-    self.SetAnswer(answer[0]+str(str_res))
+    str_res=mf.GetResults()
+    mf.SetAnswer(answer[0]+str(str_res))
 
 
+def Test_Open(mf, file):
+    print(file)
+    Test_Question(mf)
 
-
+def Test_Save(mf, file):
+    print(file)
+    file.write('Ну вы и натестировали!...')
 
 
 if __name__=='__main__':
-    StartGUI(answer=Test1, next=Test, skip=Test)
-
+    StartGUI(answer=Test_Answer, next=Test_Question, skip=Test_Question, open=Test_Open, save=Test_Save)
