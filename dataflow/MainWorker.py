9638dc319dff52c6ca6d3050fda9c4e41b4ec4a8
polyna <you@example.com>
2018-12-14 05:38:40 +0200

    production

--- dataflow/MainWorker.py
+++ dataflow/MainWorker.py
@@ -1,57 +1,117 @@
 import xml.etree.ElementTree as ET
+import datetime
 import os
 
+
 class DataControl:
+    """
+    Controller for the dataflow of application
+    Provides listiners for MainWindow
+    """
 
-   def __init__(self):
-        self.data=[]
-        self.cur_dat=None
-        self.answer_stat=[]
-        self.dirname=''
+    def __init__(self):
+        self.data = []
+        self.cur_dat = None
+        self.answer_stat = []
+        self.dirname = ''
 
+    def XMLParser(self, file):
+        """
+        Creates the self.data as a list
+        self.cur_dat[0] - question itself
+        self.cur_dat[1] - image file name
+        self.cur_dat[2] - multy (checkboxes) for many possible answers
+        self.cur_dat[3] - list of answers to choose
+        self.cur_dat[4] - list of right answers indexes (in the list above)
+        :param file: XML test file
+        """
+        try:
+            tree = ET.parse(file)
+            root = tree.getroot()
+            # make TOPLIST (2 dimension)
+            # Children are "question. Grandchildren are "text", "imagefile", "multy", ["answers"], ["right answers"]
+            toplist = [[grandchild for grandchild in child] for child in root]
+            # function to extracts text from grangchildren (if grangchildren is a list, then makes a list of texts)
+            get_text = lambda gch: [ggch.text for ggch in gch] if len(gch) > 0 else gch.text
+            # apply lambda to the TOPLIST
+            self.data = [[*(map(get_text, child))] for child in toplist]
+        except:
+            self.data = []
 
-   def XMLParser(self, file):
-        tree = ET.parse(file)
-        root = tree.getroot()        
-        d=[[grandchild for grandchild in child] for child in root]
-        get_text=lambda ch: [gch.text for gch in ch] if len(ch)>0 else ch.text  
-        self.data=[[*map(get_text, child)] for child in d]
-
-   def StartTest(self, mf, file):
-        self.dirname=os.path.dirname(file.name)
+    def StartTest(self, mf, file):
+        """
+        Opens the test file and starts the show
+        :param mf: MainFrame of the window
+        :param file: XML file with test
+        """
+        self.answer_stat = []
         self.XMLParser(file)
-        self.answer_stat=[]
+        # check for error (try-catch in XMLParser)
+        if self.data:
+            self.dirname = os.path.dirname(file.name)
+            self.KeepOnTest(mf)
+        else:
+            mf.ErrorMessage('Невірний формат файлу або помилка при його читаннi')
+
+    def KeepOnTest(self, mf):
+        """
+        Continue the test on Next pressed
+        :param mf: MainFrame of the window
+        """
+        # show status line on every step
+        mf.SetResult('Ви пройшли {} питань з {}'.format(len(self.answer_stat), len(self.answer_stat) + len(self.data)))
+        # if no more questions then finish
+        if len(self.data) == 0:
+            ok_aw, notok_aw = self.answer_stat.count(True), self.answer_stat.count(False)
+            proc_data = ok_aw / (ok_aw + notok_aw)
+            text_data = 'Ви закінчили тестування. Правильних відповідей - {} із {}'.format(ok_aw, notok_aw + ok_aw)
+            mf.SetFinalInfo(text_data, proc_data)
+        else:
+            self.cur_dat = self.data.pop()
+            image_path = os.path.join(self.dirname, self.cur_dat[1]) if self.cur_dat[1] else None
+            mf.SetQuestion(self.cur_dat[0], image=image_path)
+            mf.SetCheckList(self.cur_dat[3], self.cur_dat[2].lower() == 'yes')
+            mf.SetAnswer()
+
+    def Answer(self, mf):
+        """
+        Shows the right answer and evaluates results on Answer pressed
+        Keep on statistics in self.answer_stats
+        :param mf: MainFrame object
+        """
+        start_line_symbol = u'\u16EB '
+        end_line_symbol = ';' + os.linesep
+        # construct the right answer strings
+        right_answer_text = end_line_symbol.join(
+            [start_line_symbol + self.cur_dat[3][int(i) - 1] for i in self.cur_dat[4]])
+        mf.SetAnswer(right_answer_text)
+        # get results form MainFrame
+        res = mf.GetResults()
+        right_res = [*map(int, self.cur_dat[4])]  # convert list of right answers from text to list of int
+        # success if  res==right_res!!!
+        self.answer_stat.append(right_res == res)
+
+    def SkipTest(self, mf):
+        """
+        On Skip pressed
+        """
+        self.answer_stat.append(False)
         self.KeepOnTest(mf)
 
- 
-   def KeepOnTest(self, mf):
-        if len(self.data)==0:
-           #finish
-           ok_aw, notok_aw=self.answer_stat.count(True), self.answer_stat.count(False)
-           proc_data=ok_aw/(ok_aw+notok_aw)
-           text_data='You have got {} right answer form {}'.format(ok_aw, notok_aw+ok_aw)
-           mf.SetFinalInfo(text_data, proc_data)
-        else:
-           self.cur_dat= self.data.pop()
-           image_path=os.path.join(self.dirname, self.cur_dat[1]) if self.cur_dat[1] else None
-           mf.SetQuestion(self.cur_dat[0], image=image_path)
-           mf.SetCheckList(self.cur_dat[3], self.cur_dat[2].lower()=='yes')
-           mf.SetAnswer()
-
-           
-   def Answer(self, mf):
-       start_line_symbol=u'\u16EB '
-       end_line_symbol=';'+os.linesep
-       right_answer_text=end_line_symbol.join([start_line_symbol+self.cur_dat[3][int(i)-1] for i in self.cur_dat[4]])
-       mf.SetAnswer(right_answer_text)
-       res=mf.GetResults()     
-       if isinstance(res, list): 
-          res=[i+1 for i,_ in enumerate(res) if _>0]
-       else: 
-          res=[res]  
-       #success if  res==right_res!!!
-       right_res=[int(i) for i in self.cur_dat[4]]
-       self.answer_stat.append(right_res==res)
-
-         
-
+    def SaveResult(self, mf, file):
+        """
+        Write results to the file on Save menu
+        :param mf: MainFrame
+        :param file: file to write. file is oped and closed in the MainFrame using 'with' operator
+        """
+        if self.answer_stat:
+            date = datetime.datetime.now().strftime("%Y-%m-%d")
+            text = '''     
+           Шановний(на) Пан(i)!      
+           Ви пройшли тестування {}.
+           Ваши результати:
+           1) Вірних відповідей- {}
+           2) Помилкових відповідей- {}
+           '''
+            text = text.format(date, self.answer_stat.count(True), self.answer_stat.count(False))
+            file.write(text)
--- interface/MainWindow.py
+++ interface/MainWindow.py
@@ -1,44 +1,52 @@
 from tkinter import *
 import tkinter.filedialog as fdialog
-import random
+import tkinter.messagebox as mbox
+import textwrap
+import webbrowser
+
 
 
 
 class MainFrame(Frame):
+    """
+    MainFrame - the first ordered container for the widgets
+    To be created and gets Listeners for RegisterListeners
+    """
 
     def __init__(self, root):
         Frame.__init__(self, root)
-        #main menu
+        #makes main menu
         self.CreateMenu(root)
-        #create screen
+        self.root=root
+        #create all frames
         self.frames=self.CreateFrames()
-        # make answers to questions list
+        # creates empty widgets (with no text)
         self.CreateCheckList()
         self.CreateQuestion()
         self.CreateAnswer()
         self.CreateResults()
-        #scale full screen
+        #scale to full screen
         self.grid(row=0, column=0, sticky=(N, S, W, E))
         self.AllRowColFlexible(self, root)
         self.grid_rowconfigure(2, weight=0)
-        #stubs
+        #register None listeners - stub
         self.RegisterListener()
 
 
 
     def CreateMenu(self, root):
-        #master
+        #basement
         menu=Menu(root)
         #first level
         self.test_menu=testmenu=Menu(menu, tearoff=0)
         aboutmenu = Menu(menu, tearoff=0)
-        #sublevel
+        #second level
         menu.add_cascade(label='Тест', menu=testmenu)
         menu.add_cascade(label='Про програму', menu=aboutmenu)
         testmenu.add_command(label='Відкрити тест', command=self.OpenFileMenu)
         testmenu.add_command(label='Запамʼятати результати тестування', command=self.SaveFileMenu, state=DISABLED)
         testmenu.add_command(label='Вихід', command=root.quit)
-        aboutmenu.add_command(label='Інформація', command=None)
+        aboutmenu.add_command(label='Інформація', command=self.InfoDialog)
         root.config(menu=menu)
 
 
@@ -70,6 +78,11 @@ class MainFrame(Frame):
 
 
     def ClearSubFrame(self, sf):
+        """
+        remove all widgets from the frame.
+        clean and neat frame will be reused for answers list and checkboxes
+        :param sf: frame object
+        """
         children=sf.grid_slaves()
         for child in children:
             child.destroy()
@@ -100,6 +113,12 @@ class MainFrame(Frame):
         self.AllRowColFlexible(self.frames['answer'])
         self.frames['answer'].grid_rowconfigure(2, weight=0)
 
+
+    def CreateResults(self):
+        self.res_label=Label(self.frames['result'], text='Для початку тестування оберить файл (в меню)')
+        self.res_label.grid(row=0, column=0, sticky=(N, W, S))
+        self.AllRowColFlexible(self.frames['result'])
+        self.frames['result'].grid_rowconfigure(2, weight=0)
 
 
     def SetCheckList(self, data=None, check=True):
@@ -139,10 +158,15 @@ class MainFrame(Frame):
         self.answ_message['text'] = self.fmtQw(data)
 
 
+    def SetResult(self, data=''):
+        self.res_label['text'] = data
+
+
     def SetFinalInfo(self, data='', proc_data=None):
         self.SetCheckList()
-        self.SetAnswer()
+        self.SetAnswer(' ')
         self.SetQuestion(data)
+        self.EnableNextButton(False)
         self.FinalViewOfQuestionFrame(True)
         self.test_menu.entryconfigure(1, state=NORMAL)
         self.MakePieChart(proc_data)
@@ -150,7 +174,7 @@ class MainFrame(Frame):
 
     def FinalViewOfQuestionFrame(self, st):
         if st:
-            self.question_lable['text'] = 'Ваши реузльтаты теста'
+            self.question_lable['text'] = 'Ваші результати тестування'
             self.question_lable['fg'] = 'blue'
         else:
             self.question_lable['text'] = 'Питання'
@@ -162,8 +186,8 @@ class MainFrame(Frame):
         grad_loss,  proc_loss = 360-grad_won, 100-proc_won
         c = Canvas(self.frames['subframe'], width=340, height=210)
         c.grid(row=0, column=0)
-        c.create_text(240, 10, fill='green', text='success={}%'.format(proc_won))
-        c.create_text(240, 200, fill='red', text='failour={}%'.format(proc_loss))
+        c.create_text(240, 10, fill='green', text='правильно={}%'.format(proc_won))
+        c.create_text(240, 200, fill='red', text='помилок={}%'.format(proc_loss))
         if proc_won and proc_loss:
             start_grad = 90 - grad_won//2
             c.create_arc((170, 30, 320, 180), fill="green", start=start_grad, extent=grad_won)
@@ -181,14 +205,14 @@ class MainFrame(Frame):
     def EnableAnswertButton(self, st):
         self.answer_button.configure(state=NORMAL if st else DISABLED)
 
-    def CreateResults(self):
-        Label(self.frames['result'], text='Результати').grid(row=0, column=0, sticky=(N, W, S))
-        self.AllRowColFlexible(self.frames['result'])
-        self.frames['result'].grid_rowconfigure(2, weight=0)
-
 
 
     def AllRowColFlexible(self, *frames):
+        """
+        Sets weights=1 to all children of the frame
+        It is necessary for responsible growing of the window
+        :param frames list of frames
+        """
         for frame in frames:
             cols, rows =frame.grid_size()
             for i in range(rows):
@@ -198,11 +222,10 @@ class MainFrame(Frame):
 
 
     def RegisterListener(self, answer=None, next=None, skip=None, open=None, save=None):
-        self.answer=answer
-        self.skip=skip
-        self.next=next
-        self.open=open
-        self.save=save
+        """
+        Main connector to plug in the listeners to Buttons And Menu commands
+        """
+        self.answer, self.skip, self.next, self.open, self.save = answer, skip, next, open, save
 
 
     def OpenFileMenu(self):
@@ -234,36 +257,79 @@ class MainFrame(Frame):
     def Save(self, file):
         if self.save: self.save(self, file)
 
+    def InfoDialog(self):
+        text='''
+        Використовуйте файл тесту у форматi XML.
+        Картинки покладiть в тому ж каталозi що й файл з тестом.
+        Краще перевiрьте файл, за допомогою сервiсiв, 
+        наприклад:  https://www.freeformatter.com. 
+        Схема файлу додається: text.xsd'''
+        text=textwrap.dedent(text)  #get rid of left indent it the text
+        top = Toplevel(self)        #show dialog - copied from stackoferflow
+        x, y = self.root.winfo_x(), self.root.winfo_y()
+        top.geometry("+{}+{}".format(x + 280, y + 120)) # put it slightly right and down from root
+        top.title("Про програму...")
+        Message(top, text=text, justify=LEFT, anchor=NW, width=600).grid(row=0, column=0, columnspan=2, padx=30)
+        run=lambda: webbrowser.open_new('https://www.freeformatter.com/xml-validator-xsd.html')
+        Button(top, text="Вихiд".center(14, ' '), command=top.destroy).grid(row=1, column=0, pady=10, padx=40)
+        Button(top, text="Перевiрити", command=run).grid(row=1, column=1, pady=10, padx=20)
+
+
 
     def GetResults(self):
+        """
+        Gets IntVars List for checkbox OR IntVar single variable for radiobuttons from self.results
+        Then extract int values using IntVar.get()
+        For checkbox creates the List of index numbers (shows which boxes are cheched).
+        For radio the List with only with one integer shows which radio is selected
+        :return: the List
+        """
         try:
-            return list(map(IntVar.get, self.results)) if isinstance(self.results, list) else self.results.get()
+            if isinstance(self.results, list):
+                checkbox_cheked_list = [*map(IntVar.get, self.results)]
+                return [i + 1 for i, j in enumerate(checkbox_cheked_list) if j > 0]
+            else:
+                return [self.results.get()]   #if radiobutons
         except AttributeError:
-            return 0
+            return []                         #if results are not ready for some unknown reasons
+
+
 
     def CheckFilledResult(self):
+        """
+        Checks if we selected any answer
+        it is used to enable Answer button
+        :return: true if selected or false if we didn't touch answers
+        """
         res=self.GetResults()
-        if isinstance(res, int):
-            res=[res]
-        if sum(res)==0:
-            return False
-        else:
-            return True
+        return sum(res)!=0
+
 
     #format question and answer
+    #cut of too long lines
     def fmtQw(self, text=''):
         return text[:800]
 
     def fmtAw(self, text=''):
         return text[:200]
 
+    def ErrorMessage(self, text=''):
+        mbox.showerror("Увага", message='Помилка', detail=text)
 
-#thisd is an entry point
-#only give it your listeners
+
+
 def StartGUI(answer=None, next=None, skip=None, open=None, save=None):
+    """
+    The entry point to the MainWindow
+    Creates a window (tkinter)
+    And MainFrame on top of it
+    Then register Listeners for MainFram
+    Finally stats the loop of events
+    :param answer, next, skip open, save are all listeners
+    """
     root=Tk()
     root.title('Тестування для ліцеїстів')
-    root.geometry('1000x550')
+    root.geometry('1000x550+150+150')
     main_frame=MainFrame(root)
     main_frame.RegisterListener(answer, next, skip, open, save)
     mainloop()
@@ -272,42 +338,3 @@ def StartGUI(answer=None, next=None, skip=None, open=None, save=None):
 
 
 
-###############
-#For Tests Only
-###############
-
-def Test_Question(mf):
-    r = random.randint(0, 10)
-    if r>7:
-        mf.SetFinalInfo(' Вы вааще не правы!')
-        return
-    questions=[(('Почему все вот так, а у рыбы хвост?'), ('Когда?', 'Где?', 'Что?', 'Когда?', 'Где?', 'Что?')),
-               (('Зачем и почему все вот так, учишь-учшиь, а ничего не понимаешь?'), ('Где?', 'Что?', 'Когда?', 'Где?', 'Что?')),
-               (('Зачем козлу борода и почему все вот так?'), ('И чо?', 'Что?', 'Где?', 'Что?', 'Когда?', 'Где?', 'Что?')),
-               (('Почему все вот так, а не так и все вот так, а не сяк или Почему все вот так, а не так и все вот так, а не сяк?'),
-                     ('Когда?', 'Где?', 'Что?', 'Когда?', 'Где?', 'Что?', 'Когда?', 'Где?', 'Что?'))]
-    mode=[True, True, False, False]
-    random.shuffle(mode)
-    random.shuffle(questions)
-    mf.SetCheckList(questions[0][1], mode[0])
-    mf.SetQuestion(questions[0][0], image='../test_im1.png')
-    mf.SetAnswer()
-
-def Test_Answer(mf):
-    answer=['Глупо ', 'Тупо ', 'Умно ', 'Нудно ']
-    random.shuffle(answer)
-    str_res=mf.GetResults()
-    mf.SetAnswer(answer[0]+str(str_res))
-
-
-def Test_Open(mf, file):
-    print(file)
-    Test_Question(mf)
-
-def Test_Save(mf, file):
-    print(file)
-    file.write('Ну вы и натестировали!...')
-
-
-if __name__=='__main__':
-    StartGUI(answer=Test_Answer, next=Test_Question, skip=Test_Question, open=Test_Open, save=Test_Save)
